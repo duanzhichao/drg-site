267d025f85ce1375074d24d8c5718079a26d348e
duanzhichao <duanzhichao2008@gmail.com>
2018-09-12 08:07:24 +0800

    默认用户和默认时间添加

--- lib/schema/data_download.ex
+++ lib/schema/data_download.ex
@@ -2,6 +2,7 @@ defmodule DrgSite.DataDownload do
   use Ecto.Schema
   import Ecto.Changeset
   alias DrgSite.DataDownload
+  alias DrgSite.Time
 
   schema "data_download" do
     field :title, :string
@@ -16,8 +17,13 @@ defmodule DrgSite.DataDownload do
   end
 
   def changeset(%DataDownload{} = data_download, attrs) do
+    attrs = add_time(attrs)
     data_download
     |> cast(attrs, [:title, :time, :notice, :schedule, :download, :up_user, :up_time])
     |> validate_required([:title, :time, :notice, :schedule, :download, :up_user, :up_time])
+  end
+
+  defp add_time(attrs) do
+    attrs = Map.put(attrs, "up_time", Time.stime_local())
   end
 end
--- lib/schema/technical_download.ex
+++ lib/schema/technical_download.ex
@@ -2,6 +2,7 @@ defmodule DrgSite.TechnicalDownload do
   use Ecto.Schema
   import Ecto.Changeset
   alias DrgSite.TechnicalDownload
+  alias DrgSite.Time
 
   schema "technical_download" do
     field :file_type, :string
@@ -13,9 +14,14 @@ defmodule DrgSite.TechnicalDownload do
   end
 
   def changeset(%TechnicalDownload{} = technical_download, attrs) do
+    attrs = add_time(attrs)
     technical_download
     |> cast(attrs, [:file_type, :file_name, :up_user, :up_time])
     |> validate_required([:file_type, :file_name, :up_user, :up_time])
+  end
+
+  defp add_time(attrs) do
+    attrs = Map.put(attrs, "up_time", Time.stime_local())
   end
 
 end
